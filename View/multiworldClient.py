4bd3c5ba8984ea927b7beb66fe7b0b7a4880ba36
JarheadHME <JarheadHME@Gmail.com>
2022-03-30 18:50:25 -0400

    Add ServerWorker class to be used for server setup The Signal 'message' will be used to send messages to the main gui thread to be displayed

--- View/multiworldClient.py
+++ View/multiworldClient.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from PySide6.QtWidgets import *
+from PySide6.QtCore import QThread, QObject, Signal, Slot
 
 from View.uiMultiworldClient import uiMultiworldClient
 from Model.serverConfig import ServerConfig
@@ -9,6 +10,31 @@ import Client.clientCommunication as clientFunctions
 
 from Model.config import Config
 
+
+class ServerWorker(QObject):
+    
+    message = Signal(str) # to communicate with parent thread
+
+    def __init__(self):
+        super(ServerWorker, self).__init__()
+        self.config = Config.get_config()
+
+
+    def run(self):
+        print("Setting Fields")
+        server_config = ServerConfig(self.config._ServerAddress, self.config._Port,
+        self.config._World_id, 'admin', 'adminPass')
+        set_up_dto = SetUpDto(1, self.config._Game_Room, None, False) # 1 = world amount, just 1 for testing to match with number assigned to input box
+        try:
+            print("Into Listener")
+            asyncio.run(clientFunctions.start_connections(server_config, set_up_dto))
+            
+        except RuntimeWarning:
+            self.send_message("Failed to Create Room")
+
+    @Slot(str)
+    def send_message(self, msg: str): # to (maybe) receive messages from the actual connection things, haven't gotten that far yet
+        self.message.emit(msg)
 
 class MultiworldClientWindow(QMainWindow):
 

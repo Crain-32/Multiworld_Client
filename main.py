f5725818c5a7f1541f9d5e3b77605c8a2190b805
Jason T <jaysc@users.noreply.github.com>
2022-04-02 00:02:59 +0100

    Configparser usage (#10)
    * Config Handling adjusted to be more Robust and Pythonic

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-
 import asyncio
+import os
+import sys
 
 from Model.serverConfig import ServerConfig
 import Client.clientCommunication as clientFunctions
 from Model.config import Config
 
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is your Project Root
+
 print("Using Development version 0.0.2.5")
 print("Loading Config...")
-config_info = Config.get_config()
+config_info = Config.get_config(sys.argv[0][:-7] or ROOT_DIR)
 server_config = ServerConfig(config_info.get_address(), config_info.get_port(),
                              config_info.get_world_id(), 'user', 'password')
 try:

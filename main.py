fe5e17e727b6e65c36b8f497be1ece66dabeead6
Crain <petereastham@gmail.com>
2022-04-02 11:11:44 -0600

    Changed Version due to the GUI update.
    Increased the second number to show a "major" update involving the GUI.
    Last number increased due to refactoring of internal logic.
    Version 0.0.2.0 will increase when we release the official multiworld version.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from Model.config import Config
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is your Project Root
 
-print("Using Development version 0.0.2.5")
+print("Using Development version 0.1.2.7")
 print("Loading Config...")
 config_info = Config.get_config(sys.argv[0][:-7] or ROOT_DIR)
 server_config = ServerConfig(config_info.get_address(), config_info.get_port(),

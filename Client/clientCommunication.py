954963d9d3c525d6088903d72753a5d34986fbad
JarheadHME <JarheadHME@Gmail.com>
2022-04-04 15:08:56 -0400

    Make `clientCommunication` a class

--- Client/clientCommunication.py
+++ Client/clientCommunication.py
@@ -18,74 +18,80 @@ from Model.config import Config
 from base_logger import logging
 logger = logging.getLogger(__name__)
 
-world_id: int = Config.get_config().get_world_id()
-game_room: str = Config.get_config().get_game_room()
-event_scanning: bool = Config.get_config().Scanner_Enabled
-disable_multiplayer: bool = Config.get_config().Disable_Multiplayer
-game_handler: ClientGameConnection = ClientGameConnection(world_id)
-
-global gui_logger_signal
-async def log(message: str) -> None:
-    global gui_logger_signal
-    if isinstance(gui_logger_signal, Signal):
-        gui_logger_signal.emit(message)
-    else:
-        logger.info(message)
+class ClientConnection:
+    world_id: int
+    game_room: str
+    event_scanning: bool
+    disable_multiplayer: bool
+    game_handler: ClientGameConnection
+    gui_logger_signal: Signal
 
 
-async def start_connections(server_config: ServerConfig, set_up_dto: SetUpDto, gui_logger_signal_source: Signal(str)) -> None:
-    global gui_logger_signal
-    gui_logger_signal = gui_logger_signal_source
-    asyncio.create_task(game_handler.connect(gui_logger_signal))
-    if not disable_multiplayer:
-        await client(server_config)
+    def __init__(self, config: Config):
+        self.world_id = config.get_world_id()
+        self.game_room = config.get_game_room()
+        self.event_scanning = config.Scanner_Enabled
+        self.disable_multiplayer = config.Disable_Multiplayer
+        self.game_handler = ClientGameConnection(self.world_id)
+    
+    async def log(self, message: str) -> None:
+        if isinstance(self.gui_logger_signal, Signal):
+            self.gui_logger_signal.emit(message)
+        else:
+            logger.info(message)
+
+    async def start_connections(self, server_config: ServerConfig, set_up_dto: SetUpDto, gui_logger_signal_source: Signal) -> None:
+        self.gui_logger_signal = gui_logger_signal_source
+        asyncio.create_task(self.game_handler.connect(self.gui_logger_signal))
+        if not self.disable_multiplayer:
+            await self.client(server_config)
 
 
-async def client(server_config: ServerConfig) -> None:
-    frame_manager = StompFrameManager(server_config)
-    try:
-        async with websockets.connect("ws://" + server_config.get_uri()) as client_websocket:
-            await log(f"Attempting to Connect to {game_room}.........")
-            await client_websocket.send(frame_manager.connect(server_config.server_ip))
-            foo = await client_websocket.recv()
-            if not foo.startswith("ERROR"):
-                await client_websocket.send(frame_manager.subscribe(f"/topic/item/{game_room}"))
-                asyncio.create_task(listen_to_server(client_websocket))
-                await log(f"Successfully connected to the Server")
-                while True:
-                    for itemDto in game_handler.get_item_to_send():
-                        await client_websocket.send(frame_manager.send_json(f"/app/item/{game_room}", json.dumps(itemDto.as_dict())))
-                        game_handler.remove_item_to_send(itemDto)
-                    if QThread.currentThread().isInterruptionRequested():
-                        break
-                    await asyncio.sleep(0)
-                # Only gotten to once the loop is broken
-                try:
-                    await client_websocket.send(frame_manager.disconnect(""))
-                    await client_websocket.close()
-                    await log("Successfully disconnected from the Server")
-                    QThread.currentThread().quit() # Tells thread to fully end
-                except Exception as e:
-                    await log(f"Error disconnecting from server:\n{e}")
-                
-            else:
-                raise ServerDisconnectWarning()
-    except ServerDisconnectWarning as sdw:
-        await log("Failed to Subscribe to the Item Queue. Bad Server URL?")
-    except Exception as e:
-        await log("Problem with Server connection, please check the status with the Server host.")
+    async def client(self, server_config: ServerConfig) -> None:
+        frame_manager = StompFrameManager(server_config)
+        try:
+            async with websockets.connect("ws://" + server_config.get_uri()) as client_websocket:
+                await self.log(f"Attempting to Connect to {self.game_room}.........")
+                await client_websocket.send(frame_manager.connect(server_config.server_ip))
+                foo = await client_websocket.recv()
+                if not foo.startswith("ERROR"):
+                    await client_websocket.send(frame_manager.subscribe(f"/topic/item/{self.game_room}"))
+                    asyncio.create_task(self.listen_to_server(client_websocket))
+                    await self.log(f"Successfully connected to the Server")
+                    while True:
+                        for itemDto in self.game_handler.get_item_to_send():
+                            await client_websocket.send(frame_manager.send_json(f"/app/item/{self.game_room}", json.dumps(itemDto.as_dict())))
+                            self.game_handler.remove_item_to_send(itemDto)
+                        if QThread.currentThread().isInterruptionRequested():
+                            break
+                        await asyncio.sleep(0)
+                    # Only gotten to once the loop is broken
+                    try:
+                        await client_websocket.send(frame_manager.disconnect(""))
+                        await client_websocket.close()
+                        await self.log("Successfully disconnected from the Server")
+                        QThread.currentThread().quit() # Tells thread to fully end
+                    except Exception as e:
+                        await self.log(f"Error disconnecting from server:\n{e}")
+                    
+                else:
+                    raise ServerDisconnectWarning()
+        except ServerDisconnectWarning as sdw:
+            await self.log("Failed to Subscribe to the Item Queue. Bad Server URL?")
+        except Exception as e:
+            await self.log("Problem with Server connection, please check the status with the Server host.")
 
 
-async def listen_to_server(client_connection) -> None:
-    async for message in client_connection:
-        a = asyncio.create_task(handle_message(message))
-        await asyncio.sleep(0)
+    async def listen_to_server(self, client_connection) -> None:
+        async for message in client_connection:
+            a = asyncio.create_task(self.handle_message(message))
+            await asyncio.sleep(0)
 
 
-async def handle_message(message) -> None:
-    if message[:7] == "MESSAGE":
-        contents = message.split("\n")
-        item_dto = ItemDto.from_dict(json.loads(contents[-1][:-1]))
-        if item_dto.sourcePlayerWorldId != world_id:
-            game_handler.push_item_to_process(item_dto)
+    async def handle_message(self, message) -> None:
+        if message[:7] == "MESSAGE":
+            contents = message.split("\n")
+            item_dto = ItemDto.from_dict(json.loads(contents[-1][:-1]))
+            if item_dto.sourcePlayerWorldId != self.world_id:
+                self.game_handler.push_item_to_process(item_dto)
 
